99a4318dd2a70b51827a84f2ab482bd2e35169d2
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-02-06 22:36:36 +0600

    Acp

---
+++ Acp.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.sin(45))
+print(math.tan(45))
+print(math.cos(45))
+
+print(math.ceil(45.490))
+print(math.floor(45.890))
+print(math.factorial(5))
+
+a = math.sin(0.5)
+b = math.cos(0.5)
+c = math.tan(0.5)
+
+print(f"Value of sin (0,5)= {a}")
+print(f"Value of cos (0,5)= {b}")
+print(f"Value of tan (0,5)= {c}")
